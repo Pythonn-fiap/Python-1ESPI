7a6bd7b76dcf0cf8df936170ae1caf773495261e
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-18 11:04:26 -0300

    aula dia 18 lambda

--- 2026_18_08/ex4.py
+++ 2026_18_08/ex4.py
@@ -2,3 +2,6 @@
 #devolva o mesmo texto em letras maiúsculas. Depois, use map para aplicar essa função a
 #toda a lista nomes = ["ana", "bruno", "carla"] 
 
+para_maiscula = lambda texto: texto.upper()
+nomes = ["ana", "bruno", "carla"]
+print(list(map(para_maiscula, nomes)))  # Saída: ['ANA', 'BRUNO', 'CARLA']
---
+++ 2026_18_08/ex5.py
@@ -0,0 +1,3 @@
+#Dada a lista numeros = [15, 42, 8, 99, 23] , use reduce com uma função lambda para
+#encontrar o maior valor da lista, sem usar a função pronta max() .
+
